ac234d0cb972137eaec7373017332b0afe842961
Victor Prado <victor-2122@hotmail.com>
2019-06-18 22:34:59 -0300

    Tela semi-pronta

---
+++ Inserir.java
@@ -0,0 +1,252 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+
+public class Inserir extends JFrame implements ActionListener  {
+   private JLabel lblTitulo,lblDuracao,lblOrcamento,lblCb,lblCb2,lblCbI,lblCbP;
+   private JTextField txtTitulo, txtDuracao, txtOrcamento;
+   private JButton btnInserir,btnVoltar;
+   private JComboBox<String> cbGa,cbA,cbI;
+   private JComboBox<String> cbP;
+   private Connection conn;
+   private String guarda[],guarda2[],guarda3[];
+   private Date dt = new Date();
+   private java.sql.Date data = new java.sql.Date (dt.getTime());;
+
+   
+   public Inserir (Connection conn){
+      super("Inserir Projetos");
+      this.conn = conn;
+      
+      lblTitulo = new JLabel ("Titulo");
+      lblDuracao = new JLabel ("Duração");
+      lblOrcamento = new JLabel ("Orçamento");
+      lblCb = new JLabel ("G. Área de Conhecimento");
+      lblCb2 = new JLabel ("Área de Conhecimento");
+      lblCbI = new JLabel ("Instituição");
+      lblCbP = new JLabel ("IDPesquisador");
+      
+      txtTitulo = new JTextField(10);
+      txtDuracao = new JTextField(10);
+      txtOrcamento = new JTextField(10);
+      
+      btnInserir = new JButton ("Salvar");
+      btnVoltar = new JButton ("Voltar");
+             
+      cbGa = new JComboBox<String>();
+      cbA = new JComboBox<String>();
+      cbI = new JComboBox<String>();
+      cbP = new JComboBox<String>();
+      cbGa.addItem("Escolha");
+      cbA.addItem("Escolha");
+      cbI.addItem("Escolha");
+      cbP.addItem("Escolha");
+      listarGrandeArea();
+      listarInstituicao();
+      
+      Container cx = getContentPane();
+      cx.setLayout(new BorderLayout());
+      JPanel pnlBtn = new JPanel(new FlowLayout());
+      JPanel pnlResto = new JPanel(new FlowLayout());
+      JPanel pnlCentro = new JPanel(new GridLayout(4,1));
+      JPanel pnlCombo1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+      JPanel pnlCombo2 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+      JPanel pnlCombo3 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+      JPanel pnlCombo4 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+      
+      pnlBtn.add(btnInserir);
+      pnlBtn.add(btnVoltar);
+      pnlResto.add(lblTitulo);
+      pnlResto.add(txtTitulo);
+      pnlResto.add(lblDuracao);
+      pnlResto.add(txtDuracao);
+      pnlResto.add(lblOrcamento);
+      pnlResto.add(txtOrcamento);
+      pnlCombo1.add(lblCb);
+      pnlCombo1.add(cbGa);
+      pnlCombo2.add(lblCb2);
+      pnlCombo2.add(cbA);
+      pnlCombo3.add(lblCbP);
+      pnlCombo3.add(cbP);
+      pnlCombo4.add(lblCbI);
+      pnlCombo4.add(cbI); 
+      pnlCentro.add(pnlCombo1);
+      pnlCentro.add(pnlCombo2);
+      pnlCentro.add(pnlCombo4);
+      pnlCentro.add(pnlCombo3);
+      cx.add(pnlResto,BorderLayout.NORTH);
+      cx.add(pnlCentro,BorderLayout.CENTER);
+      cx.add(pnlBtn,BorderLayout.SOUTH);
+      
+      btnVoltar.addActionListener(this);
+      btnInserir.addActionListener(this);
+      cbGa.addActionListener(this);
+      cbI.addActionListener(this);
+      
+      setSize(520,250);
+      setLocationRelativeTo(null);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setVisible(true);
+   }
+   
+   public void actionPerformed (ActionEvent e){
+      if(e.getSource()==btnVoltar){
+         dispose();
+         ConexaoBD bd = new ConexaoBD();
+         try{
+            Connection conn = bd.conectar();
+            new Tela(conn);
+         } 
+         catch (SQLException x){
+            x.printStackTrace();
+         }
+      }
+      else if (e.getSource()==btnInserir){
+         
+         inserir();
+         JOptionPane.showMessageDialog(null,"Projeto Cadastrado!!");
+         dispose();
+         ConexaoBD bd = new ConexaoBD();
+         try{
+            Connection conn = bd.conectar();
+            new Tela(conn);
+         } 
+         catch (SQLException v){
+            v.printStackTrace();
+         }
+      }
+      else if(e.getSource()==cbGa){
+         String dados[] = String.valueOf(cbGa.getSelectedItem()).split(" - ");
+         if(!dados[0].equalsIgnoreCase("Escolha")){
+            cbA.removeAllItems();
+            cbA.addItem("--Escolha--");
+            listarArea(dados[0]);
+         }
+      }
+      else if(e.getSource()==cbI){
+      
+         String dadis[] = String.valueOf(cbI.getSelectedItem()).split(" - ");
+         if(!dadis[0].equalsIgnoreCase("Escolha")){
+            cbP.removeAllItems();
+            cbP.addItem("Escolha");
+            listarPesquisador(dadis[0]);
+         }
+      }
+      
+      
+   }
+      
+     
+    
+  
+   
+   public void listarArea(String id){
+      String query = "SELECT DISTINCT a.nome,a.id FROM areas_conhecimento a,grandes_areas_conhecimento b WHERE a.grandes_areas_conhecimento_id = ? ";
+      
+      try (PreparedStatement stm = conn.prepareStatement(query);) {
+         stm.setInt(1, Integer.parseInt(id));
+         try (ResultSet rs = stm.executeQuery();) {
+               
+            while(rs.next()){
+               cbA.addItem(String.valueOf(rs.getInt("id"))+ " - " +(rs.getString("nome")));
+            }
+         
+         }catch(Exception e){
+            e.printStackTrace();
+         }
+      }catch (SQLException e1) {
+         System.out.print(e1.getStackTrace()+e1.getMessage());
+      
+      }
+   
+   }
+   public void listarGrandeArea(){
+      String query = "SELECT DISTINCT * FROM grandes_areas_conhecimento";
+      
+      try (PreparedStatement stm = conn.prepareStatement(query);){
+            
+         ResultSet rs = stm.executeQuery();
+               
+         while(rs.next()){
+               
+            cbGa.addItem(String.valueOf(rs.getInt("id"))+ " - " +(rs.getString("nome")));
+         }
+         
+      
+      }catch(Exception e){
+         e.printStackTrace(); e.getMessage();
+      }
+         
+   }
+   
+   public void listarInstituicao(){
+      String query = "SELECT DISTINCT * FROM instituicao";
+      
+      try (PreparedStatement stm = conn.prepareStatement(query);){
+            
+         ResultSet rs = stm.executeQuery();
+               
+         while(rs.next()){
+            cbI.addItem(String.valueOf(rs.getInt("id"))+ " - " +(rs.getString("nome")));
+         }
+         
+      
+      }catch(Exception e){
+         e.printStackTrace(); e.getMessage();
+      }
+         
+   }
+   public void listarPesquisador(String id){
+      String query = "SELECT id,nome FROM pesquisadores  WHERE instituicao_id = ? ";  
+      try (PreparedStatement stm = conn.prepareStatement(query);) {
+         stm.setInt(1, Integer.parseInt(id));
+         try (ResultSet rs = stm.executeQuery();) {
+               
+            while(rs.next()){
+               cbP.addItem(String.valueOf(rs.getInt("id"))+ " - " +(rs.getString("nome")));
+            }
+         
+         }catch(Exception e){
+            e.printStackTrace();
+         }
+      }catch (SQLException e1) {
+         System.out.print(e1.getStackTrace()+e1.getMessage());
+      
+      }
+         
+   }
+   public void inserir(){
+      String sqlInsert = 
+         "INSERT INTO projetos(titulo, duracao, orcamento,data_envio,data_resposta, resposta, pesquisadores_id, instituicao_id, avaliadores_id,areas_conhecimento_id) VALUES (?, ?, ?, ?, null, null, ?, ?,null,?)";
+      guarda = String.valueOf(cbI.getSelectedItem()).split(" - ");
+      guarda2 = String.valueOf(cbP.getSelectedItem()).split(" - ");
+      guarda3 = String.valueOf(cbA.getSelectedItem()).split(" - ");
+      try (PreparedStatement stm = conn.prepareStatement(sqlInsert);) {
+         stm.setString(1, txtTitulo.getText());
+         stm.setString(2, txtDuracao.getText());
+         stm.setString(3,txtOrcamento.getText());
+         stm.setDate(4,data);
+         
+         GrandeAreaDeConhecimento g = new GrandeAreaDeConhecimento();
+         stm.setInt(5, Integer.parseInt(guarda2[0]));
+         stm.setInt(6, Integer.parseInt(guarda[0]));
+         stm.setInt(7, Integer.parseInt(guarda3[0]));
+         stm.execute();
+      
+      } catch (Exception e) {
+         e.printStackTrace(); e.getMessage();
+         try {
+            conn.rollback();
+         } catch (SQLException e1) {
+            System.out.print(e1.getStackTrace() +e1.getMessage());
+         }
+      }
+   }
+
+}
---
+++ Tela.java
@@ -0,0 +1,218 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.ListSelectionEvent;
+
+public class Tela extends JFrame implements ActionListener,ListSelectionListener{
+
+   final int LARGURA_TELA = 600;
+   final int ALTURA_TELA = 300;
+   final int LARGURA_SCROLL_PANE = LARGURA_TELA - 200;
+   final int ALTURA_SCROLL_PANE = ALTURA_TELA - 110;
+   private String[] colunas = {"ID", "Titulo", "Área de Conhecimento", "Duração","Orçamento"};
+   private Object[][] projetos;
+   
+   
+   private Container caixa;
+   private Integer[] pedidos;
+   private JTable tabelaProjetos;
+   private JPanel pnlCentro,pnlBtn,pnlLbl;
+   private JScrollPane rolagem;
+   private JButton btnExcluir,btnAlterar,btnInserir;
+   private JLabel lblProjeto;
+   private Connection conn;
+
+   
+   public Tela (Connection conn){
+      super("Menu");
+      this.conn = conn;
+      instanciaJTableEScrollPane(conn);
+      btnExcluir = new JButton ("Excluir");
+      btnInserir = new JButton("Inserir Projetos");
+      btnAlterar = new JButton("Alterar");
+      lblProjeto = new JLabel("Projetos");
+      
+      
+      caixa = getContentPane();
+      caixa.setLayout(new BorderLayout());
+      pnlCentro = new JPanel(new FlowLayout());
+      pnlBtn = new JPanel(new FlowLayout());
+      pnlLbl = new JPanel(new FlowLayout());
+      pnlLbl.add(lblProjeto);
+      pnlBtn.add(btnInserir);
+      pnlBtn.add(btnAlterar);
+      pnlBtn.add(btnExcluir);
+      pnlCentro.add(rolagem);
+      caixa.add(pnlCentro, BorderLayout.CENTER);
+      caixa.add(pnlBtn,BorderLayout.SOUTH);
+      caixa.add(pnlLbl,BorderLayout.NORTH);
+      
+      btnExcluir.addActionListener(this);
+      btnInserir.addActionListener(this);
+      btnAlterar.addActionListener(this);
+      
+      setSize(LARGURA_TELA, ALTURA_TELA);
+      setLocationRelativeTo(null);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setVisible(true);
+      
+   }
+   public void actionPerformed(ActionEvent e){
+      conn = this.conn;
+      if(e.getSource()==btnInserir){
+         dispose();
+         Inserir i = new Inserir(conn);
+      }
+      else if(e.getSource()==btnAlterar){
+         dispose();
+         Alterar a = new Alterar(conn,tabelaProjetos.getValueAt(tabelaProjetos.getSelectedRow(),0)+"");
+      }
+      else if(e.getSource()==btnExcluir){
+         int i = JOptionPane.showOptionDialog(null, "Tem certeza que deseja excluir o projeto "+tabelaProjetos.getValueAt(tabelaProjetos.getSelectedRow(),1)+"?", "ATENÇÃO",JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE,null,null,null); 
+         if (i == JOptionPane.YES_OPTION) { 
+            excluirProjeto(conn,tabelaProjetos.getValueAt(tabelaProjetos.getSelectedRow(),0)+"");
+            JOptionPane.showMessageDialog(null,"Projeto excluido com sucesso!!");
+            caixa.remove(pnlCentro);
+            pnlCentro.remove(rolagem);
+            instanciaJTableEScrollPane(conn); 
+            pnlCentro.add(rolagem);
+            caixa.add(pnlCentro, BorderLayout.CENTER);
+            validate();
+            repaint();
+         } 
+      }
+   
+   }
+   public void valueChanged(ListSelectionEvent e){
+      /* colocar dentro deste if porque o evento e disparado duas vezes e assim 
+         filtra-se somente um deles */
+      if(e.getValueIsAdjusting()){
+         String resultado = 
+            tabelaProjetos.getColumnName(0)+": "+
+            tabelaProjetos.getValueAt(tabelaProjetos.getSelectedRow(),0)+
+            "\n"+tabelaProjetos.getColumnName(1)+": "+
+            tabelaProjetos.getValueAt(tabelaProjetos.getSelectedRow(),1)+
+            "\n"+tabelaProjetos.getColumnName(2)+": "+
+            tabelaProjetos.getValueAt(tabelaProjetos.getSelectedRow(),2)+
+            "\n"+tabelaProjetos.getColumnName(3)+": "+
+            tabelaProjetos.getValueAt(tabelaProjetos.getSelectedRow(),3)+
+            "\n"+tabelaProjetos.getColumnName(4)+": "+
+            tabelaProjetos.getValueAt(tabelaProjetos.getSelectedRow(),4);
+            
+         JOptionPane.showMessageDialog(this, resultado);
+      }
+   }
+   public String[][] carregaDados(Connection conn){
+      
+      ArrayList<Projeto> lista = listar(conn);
+
+      DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+      
+      String[][] saida = new String[lista.size()][colunas.length];
+      Projeto projeto;
+      for(int i = 0; i < lista.size(); i++){
+         projeto = lista.get(i);
+         
+         saida[i][0] = projeto.getId()+"";
+         saida[i][1] = projeto.getTitulo();
+         saida[i][2] = listarArea(conn,projeto.getAreaDeConhecimento());
+         saida[i][3] = projeto.getDuracao();
+         //formata o numero com 2 casas decimais
+         saida[i][4] = String.format("%.2f", projeto.getOrcamento());
+         
+      }
+      
+      return saida;
+   }
+   //metodo para centralizar a instanciacao da JTable e nao ficar repetindo codigo
+   public void instanciaJTableEScrollPane(Connection conn){
+      
+      //carrega a matriz de pedidos para instanciar a JTable
+      projetos = carregaDados(this.conn);
+      tabelaProjetos = new JTable(projetos, colunas);
+      tabelaProjetos.getSelectionModel().addListSelectionListener(this);
+      //coloca a JTable em um scroll pane para ter a barra de rolagem
+      rolagem = new JScrollPane(tabelaProjetos);
+      rolagem.setPreferredSize(new Dimension(LARGURA_SCROLL_PANE, 
+                              ALTURA_SCROLL_PANE));
+   }
+   public ArrayList<Projeto> listar(Connection conn){
+      String query = "SELECT * FROM projetos";
+      ArrayList<Projeto> p = new ArrayList<Projeto>();
+      
+      try (PreparedStatement stm = conn.prepareStatement(query);){
+            
+         ResultSet rs = stm.executeQuery();
+               
+         while(rs.next()){
+            Projeto pro = new Projeto();
+            pro.setId(rs.getInt("id"));
+            pro.setTitulo(rs.getString("titulo"));
+            pro.setDuracao(rs.getString("duracao"));
+            pro.setOrcamento(rs.getDouble("orcamento"));
+               
+            pro.setAreaDeConhecimento(rs.getInt("areas_conhecimento_id"));
+            p.add(pro);
+         }
+         
+      
+      }catch(Exception e){
+         e.printStackTrace();
+      }
+         
+      return p;
+   }
+   public String listarArea(Connection conn, int id){
+      String query = "SELECT nome FROM areas_conhecimento  WHERE id = ?";
+      String area = "";
+      
+      try (PreparedStatement stm = conn.prepareStatement(query);) {
+         stm.setInt(1, id);
+         try (ResultSet rs = stm.executeQuery();) {
+               
+            while(rs.next()){
+               area = rs.getString("nome");
+              
+            
+            }
+         
+         }catch(Exception e){
+            e.printStackTrace();
+         }
+      }catch (SQLException e1) {
+         System.out.print(e1.getStackTrace()+e1.getMessage());
+      
+      }
+         
+      return area;
+   }
+   public void excluirProjeto(Connection conn,String id) {
+      String query = 
+         "DELETE FROM projetos WHERE id = ?";
+   
+      try (PreparedStatement stm = conn.prepareStatement(query);) {
+      
+         stm.setInt(1,Integer.parseInt(id));
+      
+         stm.execute();
+      } catch (Exception e) {
+         e.printStackTrace();
+         try {
+            conn.rollback();
+         } catch (SQLException e1) {
+            System.out.print(e1.getStackTrace());
+         }
+      }
+   }
+   
+   
+}
